4198f125546eb20dec1c1c6a0456dee377025b8a
Alan Cowap <AlanCowap@users.noreply.github.com>
2015-03-18 13:16:34 +0000

    Create SmartApp.java

---
+++ SmartApp.java
@@ -0,0 +1,35 @@
+/** 
+ * Sample code to demonstrate Inheritance, method overriding
+ * 
+ * 
+ *	 
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies none
+ *  
+ *  
+ */ 
+
+
+//package com.alancowap.cag.testing;
+
+
+class SmartApp{
+	public static void main(String[] args){
+		System.out.println("1) Enter details at specific position");
+		System.out.println("2) Print all details");
+		System.out.println("3) Calculate total screensize");
+		System.out.println("4) Quit");
+
+		Phone[] phones = new Phone[50];
+		Tablet[] tablets = new Tablet[50];
+	}
+}
+
+
+class Phone{
+}
+
+class Tablet{
+}
+
