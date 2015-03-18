ff6ccce5e9e290b91560e23a2b4a2109a666d79b
Alan Cowap <AlanCowap@users.noreply.github.com>
2015-03-18 13:53:06 +0000

    Add menu loop

--- SmartApp.java
+++ SmartApp.java
@@ -17,26 +17,29 @@ import java.io.Console;
 
 class SmartApp{
 	public static void main(String[] args){
-		// Display menu
-		System.out.println("1) Enter details at specific position");
-		System.out.println("2) Print all details");
-		System.out.println("3) Calculate total screensize");
-		System.out.println("4) Quit");
-
-		//Get user choice
-		Console con = System.console();
-		String choice = con.readLine("Please enter an option: ");
-		//System.out.println(choice);
-
-		//Evaluate user choice
-		switch(choice){
-			case "1" :	System.out.println(choice); break;
-			case "2" :	System.out.println(choice); break;
-			case "3" :	System.out.println(choice); break;
-			case "4" :	System.out.println(choice); break;
-			default	 :	System.out.println("Invalid choice"); break;
-		}
-
+		String choice = "Not Applicable";
+		do{
+			// Display menu
+			System.out.println("1) Enter details at specific position");
+			System.out.println("2) Print all details");
+			System.out.println("3) Calculate total screensize");
+			System.out.println("4) Quit");
+	
+			//Get user choice
+			Console con = System.console();
+			choice = con.readLine("Please enter an option: ");
+			//System.out.println(choice);
+	
+			//Evaluate user choice
+			switch(choice){
+				case "1" :	System.out.println(choice); break;
+				case "2" :	System.out.println(choice); break;
+				case "3" :	System.out.println(choice); break;
+				case "4" :	System.out.println("Goodbye!"); break;
+				default	 :	System.out.println("Invalid choice"); break;
+			}//switch
+		}while(!choice.equals("4"));
+		
 
 		Phone[] phones = new Phone[50];
 		Tablet[] tablets = new Tablet[50];
