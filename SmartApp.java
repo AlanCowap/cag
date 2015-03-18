a3dfba8a91cdf4716841b43838876b8643ba8866
Alan Cowap <AlanCowap@users.noreply.github.com>
2015-03-18 13:39:25 +0000

    Implement menu

--- SmartApp.java
+++ SmartApp.java
@@ -13,13 +13,30 @@
 
 //package com.alancowap.cag.testing;
 
+import java.io.Console;
 
 class SmartApp{
 	public static void main(String[] args){
+		// Display menu
 		System.out.println("1) Enter details at specific position");
 		System.out.println("2) Print all details");
 		System.out.println("3) Calculate total screensize");
 		System.out.println("4) Quit");
+
+		//Get user choice
+		Console con = System.console();
+		String choice = con.readLine("Please enter an option: ");
+		//System.out.println(choice);
+
+		//Evaluate user choice
+		switch(choice){
+			case "1" :	System.out.println(choice); break;
+			case "2" :	System.out.println(choice); break;
+			case "3" :	System.out.println(choice); break;
+			case "4" :	System.out.println(choice); break;
+			default	 :	System.out.println("Invalid choice"); break;
+		}
+
 
 		Phone[] phones = new Phone[50];
 		Tablet[] tablets = new Tablet[50];
