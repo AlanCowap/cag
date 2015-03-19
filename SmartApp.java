1873ebcd5450e0b1ff23c25da72a5f3744f7e328
Alan Cowap <AlanCowap@users.noreply.github.com>
2015-03-19 09:53:27 +0000

    Update SmartApp.java
    Add method outputDetails()

--- SmartApp.java
+++ SmartApp.java
@@ -23,6 +23,12 @@ class SmartApp{
 	}
 
 	private void runProgram(){
+		//Create & Populate Array		
+		for(int i=0; i < 100; ++i){
+			devices[i] = new Phone("Samsumg", "S6", 6, "1880000000");
+			devices[++i] = new Tablet("Samsumg", "Tab4", 10);
+		}
+
 		String choice = "Not Applicable";
 		do{
 			// Display menu
@@ -39,24 +45,22 @@ class SmartApp{
 			//Evaluate user choice
 			switch(choice){
 				case "1" :	System.out.println(choice); break;
-				case "2" :	System.out.println(choice); break;
+				case "2" :	this.outputDetails(); break;
 				case "3" :	System.out.println(choice); break;
 				case "4" :	System.out.println("Goodbye!"); break;
 				default	 :	System.out.println("Invalid choice"); break;
 			}//switch
 		}while(!choice.equals("4"));
 		
-		//Create & Populate Array		
-		for(int i=0; i < 100; ++i){
-			devices[i] = new Phone("Samsumg", "S6", 6, "1880000000");
-			devices[++i] = new Tablet("Samsumg", "S6", 6);
-		}
 
+	
+	}
+
+	private void outputDetails(){
 		//Output Device details				
 		for(int i=0; i < 100; ++i){
 			System.out.println(devices[i] +" ");
 		}
-	
 	}
 
 }
@@ -74,6 +78,7 @@ class Device{
 		this.screenSize = screenSize;
 	}
 
+	//@Override
 	public String toString(){
 		return (manufacturer +" "+ model +" "+ screenSize);
 	}
@@ -87,6 +92,8 @@ class Phone extends Device{
 		super(manufacturer, model, screenSize);
 		this.phoneNumber = phoneNumber;
 	}
+
+	//@Override
 	public String toString(){
 		return super.toString() + " "+ phoneNumber;
 	}
